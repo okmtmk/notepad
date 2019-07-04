ba4985d3e833e2fe5fee380ab9944b6ea7e8df1c
okmtmk <c0118060b4@edu.teu.ac.jp>
2019-07-04 18:05:46 +0900

    [add]クリエイト文書いた

--- app/src/main/java/jp/okmt/notepad/database/MemoDatabaseHelper.kt
+++ app/src/main/java/jp/okmt/notepad/database/MemoDatabaseHelper.kt
@@ -5,11 +5,21 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 class MemoDatabaseHelper(context: Context) : SQLiteOpenHelper(context, "memo.db", null, 1) {
+
+    companion object {
+        const val TABLE_NAME = "memos"
+        const val ID = "_id"
+        const val TITLE = "title"
+        const val FILEPATH = "filepath"
+        const val CREATED_AT = "created_at"
+        const val UPDATED_AT = "updated_at"
+    }
+
     override fun onCreate(db: SQLiteDatabase) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        db.execSQL("create table $TABLE_NAME ($ID integer primary key, $TITLE text not null, $FILEPATH text not null unique, $CREATED_AT text not null, $UPDATED_AT text not null);")
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //To change body of created functions use File | Settings | File Templates.
     }
 }
